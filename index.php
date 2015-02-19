0fcc9f76672b644a5acb0eb48bc009d645bd2845
Lloyd Cledwyn <cledwyn@gmail.com>
2015-02-18 20:51:43 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+	<title>Simple Form</title>
+</head>
+
+<body>
+<h1>SEIS752 Advanced Web Application Development<br />
+  Simple Forms
+</h1>
+<form action="/x/y/z" method="POST"> 
+    Value1: <input type="text" name="value1"/><br /> 
+    Value2: <input type="text" name="value2" value="47"/><br /> 
+    <input type="submit" value="Submit"/> 
+</form> 
+
+</body>
+</html>
